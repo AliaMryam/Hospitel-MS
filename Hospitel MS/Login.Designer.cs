490417da69e4398a27536b6aeff8fbc471085243
aliam <aliam@ALIA>
2024-06-28 14:38:26 +0500

    Add project files.

---
+++ Hospitel MS/Login.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Hospitel_MS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Adminbtn = new System.Windows.Forms.Label();
+            this.Passwordbtn = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Loginbtn = new System.Windows.Forms.Button();
+            this.Closebtn = new System.Windows.Forms.Button();
+            this.Clrebtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 104);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hospital Management System\r\n\r\n\r\n\r\n";
+            // 
+            // Adminbtn
+            // 
+            this.Adminbtn.AutoSize = true;
+            this.Adminbtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adminbtn.Location = new System.Drawing.Point(3, 195);
+            this.Adminbtn.Name = "Adminbtn";
+            this.Adminbtn.Size = new System.Drawing.Size(64, 22);
+            this.Adminbtn.TabIndex = 1;
+            this.Adminbtn.Text = "Admin";
+            // 
+            // Passwordbtn
+            // 
+            this.Passwordbtn.AutoSize = true;
+            this.Passwordbtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Passwordbtn.Location = new System.Drawing.Point(3, 243);
+            this.Passwordbtn.Name = "Passwordbtn";
+            this.Passwordbtn.Size = new System.Drawing.Size(89, 22);
+            this.Passwordbtn.TabIndex = 2;
+            this.Passwordbtn.Text = "Password";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hospitel_MS.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(94, 184);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(232, 33);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(98, 243);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(232, 33);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Loginbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loginbtn.Location = new System.Drawing.Point(108, 323);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.Size = new System.Drawing.Size(75, 44);
+            this.Loginbtn.TabIndex = 6;
+            this.Loginbtn.Text = "Login";
+            this.Loginbtn.UseVisualStyleBackColor = false;
+            this.Loginbtn.Click += new System.EventHandler(this.Loginbtn_Click);
+            // 
+            // Closebtn
+            // 
+            this.Closebtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Closebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Closebtn.Location = new System.Drawing.Point(329, 5);
+            this.Closebtn.Name = "Closebtn";
+            this.Closebtn.Size = new System.Drawing.Size(40, 36);
+            this.Closebtn.TabIndex = 7;
+            this.Closebtn.Text = "X";
+            this.Closebtn.UseVisualStyleBackColor = false;
+            this.Closebtn.Click += new System.EventHandler(this.Closebtn_Click);
+            // 
+            // Clrebtn
+            // 
+            this.Clrebtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Clrebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clrebtn.Location = new System.Drawing.Point(108, 398);
+            this.Clrebtn.Name = "Clrebtn";
+            this.Clrebtn.Size = new System.Drawing.Size(75, 44);
+            this.Clrebtn.TabIndex = 8;
+            this.Clrebtn.Text = "Clear";
+            this.Clrebtn.UseVisualStyleBackColor = false;
+            this.Clrebtn.Click += new System.EventHandler(this.Clrebtn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(372, 497);
+            this.Controls.Add(this.Clrebtn);
+            this.Controls.Add(this.Closebtn);
+            this.Controls.Add(this.Loginbtn);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Passwordbtn);
+            this.Controls.Add(this.Adminbtn);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Adminbtn;
+        private System.Windows.Forms.Label Passwordbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button Loginbtn;
+        private System.Windows.Forms.Button Closebtn;
+        private System.Windows.Forms.Button Clrebtn;
+    }
+}
+
---
+++ Hospitel MS/Login.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospitel_MS
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clrebtn_Click(object sender, EventArgs e)
+        {
+            Adminbtn.Text = "";
+            Passwordbtn.Text = "";
+        }
+
+        private void Closebtn_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            if(Adminbtn.Text==""&&Passwordbtn.Text=="")
+            {
+                MessageBox.Show("Missing Information");
+            }
+            else if(Adminbtn.Text=="Admin"&&Passwordbtn.Text=="Password") 
+            {
+                Home obj= new Home();
+                obj.Show();
+                this.Hide();
+            } 
+             
+            
+        }
+    }
+}
